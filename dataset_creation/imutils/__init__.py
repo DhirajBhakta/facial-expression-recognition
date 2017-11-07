f27cb80784f5c2e4d41c34a292159a427a93b97c
DhirajBhakta <dhirajbhakta110@gmail.com>
2017-11-07 09:45:17 +0530

    import bug fix. name of class changed from FaceAligner to FacePreProcessor

--- dataset_creation/imutils/__init__.py
+++ dataset_creation/imutils/__init__.py
@@ -4,7 +4,7 @@ from .manip import rotate
 from .manip import translate
 from .manip import resize
 
-from .faceAligner import FaceAligner
+from .facePreProcessor import FacePreProcessor
 
 from .face_utils import rect_to_bb
 from .face_utils import shape_to_np
